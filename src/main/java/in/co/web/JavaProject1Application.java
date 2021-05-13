b70fdf54d49fd6ae5eaf803d6293fb0909c04b56
deepak ughade <deepakughade200@gmail.com>
2021-05-13 15:03:41 +0530

    first commit

---
+++ src/main/java/in/co/web/JavaProject1Application.java
@@ -0,0 +1,16 @@
+package in.co.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProject1Application.class, args);
+		
+		
+		System.out.println("hello jenkins");
+	}
+
+}
